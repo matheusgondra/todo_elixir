dab2269cc0cd62e36d8fc1b26ac0de8334e07391
matheusgondra <matheusgondra951@gmail.com>
2024-12-30 16:29:17 -0300

    testando rota

--- test/todo_web/controllers/task/update_title_task_controller_test.exs
+++ test/todo_web/controllers/task/update_title_task_controller_test.exs
@@ -1,4 +1,4 @@
-defmodule TodoWeb.Task.CompleteTaskControllerTest do
+defmodule TodoWeb.Task.UpdateTitleTaskControllerTest do
   use TodoWeb.ConnCase, async: true
 
   import Todo.Factory
@@ -9,33 +9,32 @@ defmodule TodoWeb.Task.CompleteTaskControllerTest do
   describe "handle/2" do
     setup %{conn: conn} do
       user = insert(:user)
+
       {:ok, token, _claims} = Guardian.encode_and_sign(user)
+
       conn = put_req_header(conn, "authorization", "Bearer #{token}")
 
       {:ok, conn: conn, user_id: user.id}
     end
 
-    test "completes a task", %{conn: conn, user_id: user_id} do
-      id = insert(:task, %{user_id: user_id}).id
+    test "returns the updated task", %{conn: conn, user_id: user_id} do
+      task = insert(:task, %{user_id: user_id})
+      params = %{"title" => "New Title"}
 
       response =
         conn
-        |> patch(~p"/api/tasks/#{id}/complete")
+        |> patch(~p"/api/tasks/#{task.id}", params)
         |> json_response(200)
 
-      assert %{
-               "id" => ^id,
-               "title" => "any_title",
-               "completed" => true,
-               "created_at" => _,
-               "updated_at" => _
-             } = response
+      assert %{"title" => "New Title"} = response
     end
 
-    test "returns a 404 status code when the task does not exist", %{conn: conn} do
+    test "returns an error when the task is not found", %{conn: conn} do
+      params = %{"title" => "New Title"}
+
       response =
         conn
-        |> patch(~p"/api/tasks/#{UUID.generate()}/complete")
+        |> patch(~p"/api/tasks/#{UUID.generate()}", params)
         |> json_response(404)
 
       expected_response = %{"message" => "Task not found"}
