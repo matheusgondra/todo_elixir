5bb6a5f706376f39818598a51aa283e4a659a0c0
matheusgondra <matheusgondra951@gmail.com>
2024-12-30 15:01:36 -0300

    testando rota PATCH /api/tasks/:id/complete

---
+++ test/todo_web/controllers/task/complete_task_controller_test.exs
@@ -0,0 +1,48 @@
+defmodule TodoWeb.Task.CompleteTaskControllerTest do
+  use TodoWeb.ConnCase, async: true
+
+  import Todo.Factory
+
+  alias Ecto.UUID
+  alias TodoWeb.Auth.Guardian
+
+  describe "handle/2" do
+    setup %{conn: conn} do
+      user = insert(:user)
+      {:ok, token, _claims} = Guardian.encode_and_sign(user)
+      conn = put_req_header(conn, "authorization", "Bearer #{token}")
+
+      {:ok, conn: conn, user_id: user.id}
+    end
+
+    test "completes a task", %{conn: conn, user_id: user_id} do
+      id = insert(:task, %{user_id: user_id}).id
+
+      IO.inspect(id, label: "id da task")
+
+      response =
+        conn
+        |> patch(~p"/api/tasks/#{id}/complete")
+        |> json_response(200)
+
+      assert %{
+               "id" => ^id,
+               "title" => "any_title",
+               "completed" => true,
+               "created_at" => _,
+               "updated_at" => _
+             } = response
+    end
+
+    test "returns a 404 status code when the task does not exist", %{conn: conn} do
+      response =
+        conn
+        |> patch(~p"/api/tasks/#{UUID.generate()}/complete")
+        |> json_response(404)
+
+      expected_response = %{"message" => "Task not found"}
+
+      assert response == expected_response
+    end
+  end
+end
